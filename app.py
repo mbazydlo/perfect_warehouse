d6d27da9c1c6edea9ffc405aee70eb3c13f32fb5
Bazydło Marcin <marcin.p.bazydlo@gmail.com>
2020-04-13 22:43:31 +0200

    v0.0.4 Dodany routing do warehouseMain.html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-
-
+@app.route('/')
+def warehouseMain():
+    return render_template('warehouseMain.html', message = 'To je Main')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
